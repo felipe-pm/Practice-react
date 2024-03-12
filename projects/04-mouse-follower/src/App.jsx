c24095351fc9cdb1dafa6bd9e966acf801de2af1
felipe-pm <pipeeen@gmail.com>
2024-03-12 18:40:52 -0300

    Se suben los avances del mouse follower

--- projects/04-mouse-follower/src/App.jsx
+++ projects/04-mouse-follower/src/App.jsx
@@ -1,8 +1,65 @@
+import { useEffect, useState } from "react"
 
-function App() {
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    console.log('effect', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('handleMove', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    // cleanup
+    // Cuando el componente se desmonta
+    // Cuando cambian las dependencias, antes de ejecutar el efecto de nuevo
+    return () => {
+      console.log('cleanup')
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
 
   return (
-    <h3>Mouse Follower</h3>
+    <>
+      <div
+        style={{
+          position: 'absolute',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          border: '1px solid white',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: '-50px',
+          top: '-50px',
+          width: '100px',
+          height: '100px',
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      />
+
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </>
+  )
+}
+
+function App() {
+  const [mounted, setMounted] = useState(true)
+  return (
+    <main>
+      {mounted && <FollowMouse />}
+      <button onClick={() => setMounted(!mounted)}>
+        Toggle mounted FollowMouse component
+      </button>
+    </main>
   )
 }
 
